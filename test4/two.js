5cda448a421212951157a3d0fa3d0fdbe284e4c2
yerxio <62973094+yerxio@users.noreply.github.com>
2023-10-16 00:36:17 +0800

    Delete test4 directory

--- test4/two.js
+++
@@ -1,9 +0,0 @@
-// url = 'ignoreURLParametersMatching'
-function get_authkey(url){
-    const precachedURL = getCacheKeyForURL(url, {
-      cleanURLs,
-      directoryIndex,
-      ignoreURLParametersMatching,
-      null,
-    }
-};
